28e78b7059e1d4c5fe939b793f78fd12cb35ad23
vingi <vingi520@msn.com>
2012-11-20 00:04:49 +0800

    mo
    Signed-off-by: vingi <vingi520@msn.com>

--- MVCBase/Areas/SuperAdmin/Controllers/DesignerController.cs
+++ MVCBase/Areas/SuperAdmin/Controllers/DesignerController.cs
@@ -24,23 +24,42 @@ namespace MVCBase.Areas.SuperAdmin.Controllers
             DAL.DesignType dal_designtype = new DAL.DesignType();
             var designtype = dal_designtype.GetAllModel();
             ViewBag.designtype = designtype;
-
+            //設計風格
+            DAL.DesignStyleType dal_designstyletype = new DAL.DesignStyleType();
+            var designstyletype = dal_designstyletype.GetAllModel();
+            ViewBag.designstyletype = designstyletype;
 
             return View(model);
         }
 
         [HttpPost,ValidateInput(false)]
-        public ActionResult Submit()
+        public ActionResult Submit(Designer_Form form)
         {
+
             return View();
         }
     }
 
     public class Designer_Form
     {
-            public int designer_id { get; set; }
-            public string designer_title { get; set; }
-            public string designer_subtitle { get; set; }
-            public string designer_description { get; set; }
+        public int designer_id { get; set; }
+        public string designer_name { get; set; }
+        public string designer_company { get; set; }
+        public string designer_tel { get; set; }
+        public string designer_email { get; set; }
+        public string designer_address { get; set; }
+        public string designer_url { get; set; }
+        public string designer_local { get; set; }
+        public string[] designer_htype { get; set; }
+        public string[] designer_dtype { get; set; }
+        public string[] designer_stype { get; set; }
+        public bool designer_entrust { get; set; }
+        public bool designer_decoration { get; set; }
+        public string designer_price { get; set; }
+        public string designer_strengths { get; set; }
+        public string designer_text { get; set; }
+        public bool designer_pdesign { get; set; }
+        public bool designer_ndesign { get; set; }
+        public bool designer_sdesign { get; set; }
     }
 }
