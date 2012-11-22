36911139e4fab9c33966579e587c328f49262008
vingi <vingi520@msn.com>
2012-11-22 23:53:49 +0800

    modify
    Signed-off-by: vingi <vingi520@msn.com>

--- MVCBase/Controllers/DesignerController.cs
+++ MVCBase/Controllers/DesignerController.cs
@@ -11,9 +11,25 @@ namespace MVCBase.Controllers
         //
         // GET: /Designer/
 
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            return View();
+            DAL.Designer dal = new DAL.Designer();
+            var model = dal.GetSingledataById(id.HasValue ? int.Parse(id.ToString()) : 0);
+
+            //房屋類型
+            DAL.HouseType dal_housetype = new DAL.HouseType();
+            var housetype = dal_housetype.GetAllModel();
+            ViewBag.housetype = housetype;
+            //設計型式
+            DAL.DesignType dal_designtype = new DAL.DesignType();
+            var designtype = dal_designtype.GetAllModel();
+            ViewBag.designtype = designtype;
+            //設計風格
+            DAL.DesignStyleType dal_designstyletype = new DAL.DesignStyleType();
+            var designstyletype = dal_designstyletype.GetAllModel();
+            ViewBag.designstyletype = designstyletype;
+
+            return View(model);
         }
 
     }
